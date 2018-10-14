1fe249b4c8057b68fcf4c4996fb2e02d2cadd20d
Liliana Terry <lilianaterry@gmail.com>
2018-10-14 16:54:22 -0500

    setting up class structure

---
+++ src/commands.js
@@ -0,0 +1,23 @@
+import { Database } from './table_operations';
+
+export class Commands {
+	constructor() {
+		// TODO
+	}
+
+	static startConversation(user_id: string) {
+		// save user to dynamo table
+		// prompt them for subscription suggestions
+		// ask about frequency
+		// save responses with id in dynamo table 
+		console.log("start conversation!");
+		// Database.addUser(user_id);
+	}
+
+	static endConversation(user_id: string) {
+		// reprompt to make sure they wish to leave
+		// remove all entries from dynamo table
+		// send goodbye message
+	}
+}
+
