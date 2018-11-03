253d3a8205bb85024f3ce22effd1c65f642c2429
EC2 Default User <ec2-user@ip-172-31-38-202.ec2.internal>
2018-11-03 02:17:10 +0000

    got a custom response back from fulfillment

--- src/index.js
+++ src/index.js
@@ -13,73 +13,22 @@ const app = express().use(bodyParser.json()); // creates express http server
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 8000, () => console.log('Listening for requests from Messenger'));
 
-// Creates the endpoint for our webhook
+//Creates the endpoint for our webhook
 app.post('/webhook', (req: any, res: any) => {
   console.log('Received webhook POST request');
-
-  const {
-    body,
-  } = req;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach((entry) => {
-      // Gets the message. entry messaging is an array, but
-      // will only ever contain one message, so we get index 0
-      const webhookEvent = entry.messaging[0];
-
-      console.log('*** WEBHOOK EVENT RECEIVED ***:');
-      console.log(JSON.stringify(webhookEvent, null, 2));
-
-      // Ignore extraneous messages sent with nlp analysis for now
-      if (webhookEvent && !_.get(webhookEvent, 'message.nlp') && _.get(webhookEvent, 'message')) {
-        const userId = webhookEvent.sender.id;
-
-        // user sent a text message
-        if (webhookEvent.message.text) {
-          const messageCommand = webhookEvent.message.text.toLowerCase();
-
-          // Decides which command to run from user request
-          switch (messageCommand) {
-            case 'start':
-              Commands.startConversation(userId);
-              break;
-            default:
-              console.log('Sorry that command was not found');
-          }
-
-          MessengerApi.getInstance().sendTextMessage(webhookEvent.sender.id, 'Message received');
-/*          console.log('Uploading image');
-         const imageGenerator = new ImageGenerator(2000, 2000);
-          const directory = __dirname;
-          imageGenerator.genImage().write(directory)
-            .then((imageFilename) => {
-              MessengerApi.getInstance().uploadImageAttachment(`${directory}/${imageFilename}`, imageFilename, 'image/png')
-                .then((attachmentId) => {
-                  MessengerApi.getInstance()
-                    .sendImageAttachmentWithId(webhookEvent.sender.id, attachmentId)
-                    .catch((error) => {
-                      console.error(error);
-                    });
-                  fs.unlinkSync(`${directory}/${imageFilename}`);
-                });
-            }).catch((error) => {
-              console.error(error);
-            });*/
-        } else {
-          console.log(`No text found. WebhookEvent: \n${JSON.stringify(webhookEvent, null, 4)}`);
-        }
-      }
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
+	
+	return res.json({
+		"source": "pressbotbox.com",
+		"payload": {
+			"facebook": {
+    		"text": "Hello, welcome to Pressbot!"
+  		}
+		}
+	});
+  // Returns a '200 OK' response to all requests
+  res.status(200).send('EVENT_RECEIVED');
 });
+
 
 // Adds support for GET requests to our webhook
 app.get('/webhook', (req: any, res: any) => {
