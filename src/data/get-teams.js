082c8152b6110fa12c6c7abe41359d69f1e78693
EC2 Default User <ec2-user@ip-172-31-38-202.ec2.internal>
2018-11-20 01:46:31 +0000

    queried all teams from endpoint

---
+++ src/data/get-teams.js
@@ -0,0 +1,54 @@
+const request = require('request-promise');
+const _ = require('lodash');
+const AWS = require('aws-sdk');
+
+function addTeamsToDatabase(teams) {
+  AWS.config.update({
+    region: 'us-east-1',
+  });
+
+  const table = 'dmn_teams';
+  const docClient = new AWS.DynamoDB.DocumentClient();
+
+  teams.forEach(team => {
+    const params = {
+      TableName: table,
+      Item: {
+        team_id: team.teamId,
+        team_name: team.teamName,
+      },
+    };
+
+    docClient.put(params, (err) => {
+      if (err) {
+        console.error('Unable to add item. Error JSON:', JSON.stringify(err, null, 2));
+      }
+    });
+  });
+}
+
+function queryAllTeams() {
+  TEAM_PLAYER_API = "http://belo-web1.newsengin.com/dallas/tpweb/web/gateway.php?site=default&tpl=API_ActiveTeams&contentType=json";
+
+  const options = {
+    method: 'GET',
+    uri: TEAM_PLAYER_API,
+    headers: {
+      'User-Agent': 'Request-Promise'
+    },
+    json: true,
+  };
+
+  request(options)
+    .then(function (response) {
+      // get all teams
+      const allTeams = _.flatMap(response, function (item) {
+        return [{teamId: item.TeamID, teamName: item.TeamName}];
+      });
+
+      addTeamsToDatabase(allTeams);
+    })
+    .catch(function (err) {
+      throw new Error('Get team request failed.');
+    });
+}
